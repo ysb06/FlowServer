af360abb33d724fd8674eb49433a217be122835f
ysb06 <ysb06@hotmail.com>
2018-06-11 17:58:51 +0900

    Base Code
    Base code for next step.

--- FlowServer/MainForm.Designer.cs
+++ FlowServer/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlowServer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
